849f3791f6e590e2f8da02cb27b86222f9c2a34c
Juliano Resuli <jou1993@gmail.com>
2021-01-28 15:41:14 +0200

    created NoteDao interface

---
+++ app/src/main/java/com/example/architecturecomponent/NoteDatabase.java
@@ -0,0 +1,29 @@
+package com.example.architecturecomponent;
+
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+@Database(entities=Note.class,version=1)
+public class NoteDatabase extends RoomDatabase {
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+}
